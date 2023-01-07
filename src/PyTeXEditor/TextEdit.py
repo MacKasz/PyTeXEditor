7452c44094c5e85e63f0e146abd150afbb528ed2
Maciej Kaszynski <maciekkaszynski1212@gmail.com>
2023-01-07 15:15:50 +0000

    Sidebar visibility control

--- src/PyTeXEditor/TextEdit.py
+++ src/PyTeXEditor/TextEdit.py
@@ -1,9 +1,16 @@
 from PyQt6 import QtWidgets
-from PyQt6.QtCore import pyqtSignal, Qt
+from PyQt6.QtCore import pyqtSignal
 
 
 class TextEdit(QtWidgets.QTextEdit):
 
+    sidebar_visable_signal = pyqtSignal(bool)
+
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.hide_signal = pyqtSignal(bool)
+
+    def hide_sidebar(self) -> None:
+        self.sidebar_visable_signal.emit(False)
+
+    def show_sidebar(self) -> None:
+        self.sidebar_visable_signal.emit(True)
---
+++ tests/test_textedit_hide_sidebar.py
@@ -0,0 +1,40 @@
+import pytest # noqa F401
+from PyTeXEditor.TextEdit import TextEdit
+from PyTeXEditor.Sidebar import Sidebar
+
+
+def test_send_hide(qtbot):
+    """Check if signals are sent from the textedit
+    """
+    textedit = TextEdit()
+
+    with qtbot.waitSignal(textedit.sidebar_visable_signal, timeout=100,
+                          raising=True):
+        textedit.hide_sidebar()
+
+    with qtbot.waitSignal(textedit.sidebar_visable_signal, timeout=100,
+                          raising=True):
+        textedit.show_sidebar()
+
+
+def test_sidebar_hide(qtbot):
+    """Check if the sidebar reacts upon reciving hide or show signal
+    """
+    textedit = TextEdit()
+    sidebar = Sidebar()
+    textedit.sidebar_visable_signal.connect(sidebar.setVisible)
+
+    qtbot.addWidget(textedit)
+    qtbot.addWidget(sidebar)
+
+    with qtbot.waitSignal(textedit.sidebar_visable_signal, timeout=100,
+                          raising=True):
+        textedit.hide_sidebar()
+
+    assert sidebar.isVisible() is False
+
+    with qtbot.waitSignal(textedit.sidebar_visable_signal, timeout=100,
+                          raising=True):
+        textedit.show_sidebar()
+
+    assert sidebar.isVisible() is True
