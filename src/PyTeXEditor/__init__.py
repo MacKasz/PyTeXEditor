3fff57c573bee6014e2264378ef9ada635516493
Maciej Kaszynski <maciekkaszynski1212@gmail.com>
2023-02-01 21:16:23 +0000

    mypy fix

--- src/PyTeXEditor/__init__.py
+++ src/PyTeXEditor/__init__.py
@@ -4,4 +4,4 @@ from PyTeXEditor.TextEdit import TextEdit  # noqa F401
 from PyTeXEditor.Ribbon import Ribbon  # noqa F401
 from PyTeXEditor.Menubar import Menubar  # noqa F401
 
-__all__ = ["Ribbon", "Sidebar", "TextEdit", "Window"]
+__all__ = ["Ribbon", "Sidebar", "TextEdit", "Window", "Menubar"]
