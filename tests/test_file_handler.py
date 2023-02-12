91b63b6fc0b3685890e8e688b36b9242ec92cbe3
Maciej Kaszynski <maciekkaszynski1212@gmail.com>
2023-02-12 15:02:13 +0000

    some tests

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -1,5 +1,6 @@
 import pytest # noqa F401
 from PyTeXEditor.file_handler import FileHandler
+from PyTeXEditor.document_elements import Document, Text
 from pathlib import Path
 import os
 
@@ -14,3 +15,12 @@ def test_init():
 
     handler_sym = FileHandler(resources_dir / "linked_document.tex")
     assert handler_sym.file_path == resources_dir / "actual_document.tex"
+
+    handler_sym.read_file()
+    assert handler_sym.doc.plain_text is not None
+    handler_sym.convert()
+
+    tree = handler_sym.doc.object_tree
+    assert type(tree.root.data) is Document
+
+    assert type(tree.root.children[0].data) == Text
