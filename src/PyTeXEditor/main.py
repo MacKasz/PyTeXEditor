a936e0a8649cbd12cc3eb160197ecc256d8ce10d
Maciej Kaszynski <maciekkaszynski1212@gmail.com>
2023-03-07 11:57:46 +0000

    making window default size larger

--- src/PyTeXEditor/main.py
+++ src/PyTeXEditor/main.py
@@ -1,5 +1,6 @@
 from PyTeXEditor.window import Window
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import QSize
 import sys
 import logging
 
@@ -8,4 +9,8 @@ if __name__ == "__main__":
     logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
     app = QApplication(sys.argv)
     ex = Window()
+    size = QSize()
+    size.setHeight(600)
+    size.setWidth(800)
+    ex.resize(size)
     sys.exit(app.exec())
